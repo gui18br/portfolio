07e224cfee68aefc7f1c9fddbf14bc2532551496
gui18br <guilherme@vcasistemas.com.br>
2024-06-07 12:09:42 -0300

    feat: Add react-router-dom dependency and implement basic routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Portfolio</h1>;
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./screens/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
