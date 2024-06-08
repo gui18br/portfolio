2da6fc8d17a932d6868dbad9d7fd89dfc1405138
gui18br <guilherme@vcasistemas.com.br>
2024-06-08 00:44:31 -0300

    feat: Update ProjectList component to display project images, titles, and descriptions

--- src/components/projectList/ProjectList.tsx
+++ src/components/projectList/ProjectList.tsx
@@ -8,10 +8,19 @@ export const ProjectList = () => {
       <div className="flex justify-center">
         <Title size="5xl">Projects</Title>
       </div>
-      <ul>
+      <ul className="flex flex-col items-center gap-24 mt-20 w-full">
         {projects.map((project) => (
-          <li>
-            <Link to={`/${project.id}`}>{project.title}</Link>
+          <li className="flex items-center gap-44">
+            <img src={project.img} className="w-[400px] h-[400px]" alt="" />
+            <div className="flex flex-col gap-5 w-full">
+              <h1 className="text-3xl">{project.title}</h1>
+              <p className="text-lg">{project.description}</p>
+              <div className="flex justify-end">
+                <button className="bg-primary text-[#E9E0D9] w-36 h-10 rounded-sm text-xl transition-transform duration-300 transform hover:-translate-y-1">
+                  Project
+                </button>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -3,11 +3,13 @@ const projects = [
     id: 1,
     title: "Project 1",
     description: "This is a description for project 1",
+    img: "https://via.placeholder.com/150",
   },
   {
     id: 2,
     title: "Project 2",
     description: "This is a description for project 2",
+    img: "https://via.placeholder.com/150",
   },
 ];
 
