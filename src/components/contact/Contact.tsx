e1d291be06e94effb8653c44c870dc8af4dcff51
gui18br <guilherme@vcasistemas.com.br>
2024-07-11 15:56:33 -0300

    chore: atualiza background e paleta de cores

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,6 +1,5 @@
 import { Element } from "react-scroll";
-import background from "../../assets/jpeg/backgrond-2.jpg";
-import backgroundMobile from "../../assets/jpeg/backgrond-2-mobile.jpg";
+import background from "../../assets/jpeg/bg-brown.jpg";
 import { SendEmail } from "../sendEmail/SendEmail";
 import { Title } from "../title/Title";
 
@@ -16,11 +15,11 @@ export const Contact = () => {
         alt=""
       />
       <img
-        src={backgroundMobile}
-        className="shadow-sm lg:hidden block sm:h-[600px] sm:w-full "
+        src={background}
+        className="shadow-sm lg:hidden block h-screen sm:h-[600px] sm:w-full "
         alt=""
       />
-      <div className="bg-[#e9e0d9c0] absolute rounded-md sm:p-3 lg:p-3 p-3 sm:mt-14 mt-20 shadow-xl flex flex-col items-center justify-center sm:gap-1 lg:gap-3 gap-5">
+      <div className="bg-[#e9e0d9ea] absolute rounded-md sm:p-3 lg:p-3 p-3 sm:mt-14 mt-20 shadow-xl flex flex-col items-center justify-center sm:gap-1 lg:gap-3 gap-5">
         <div className="p-0">
           <Title size="5xl">Me contate</Title>
         </div>
--- src/components/headerProject/HeaderProject.tsx
+++ src/components/headerProject/HeaderProject.tsx
@@ -1,7 +1,6 @@
 import { NavBar } from "../navBar/NavBar";
-import background from "../../assets/jpeg/background.jpg";
-import backgroundMobile from "../../assets/jpeg/background-mobile.jpg";
 import { Button } from "../button/Button";
+import background from "../../assets/jpeg/bg-brown.jpg";
 
 interface HeaderProjectProps {
   title: string;
@@ -18,21 +17,12 @@ export const HeaderProject = (props: HeaderProjectProps) => {
   return (
     <div className="relative">
       <NavBar />
-      <img
-        src={background}
-        className="w-full shadow-sm hidden lg:block xl:h-[750px]"
-        alt=""
-      />
-      <img
-        src={backgroundMobile}
-        className="shadow-sm lg:hidden block sm:h-[900px] sm:w-full "
-        alt=""
-      />
-      <div className="absolute sm:bottom-20 lg:bottom-36 xl:bottom-32 bottom-20 w-full h-full flex flex-col justify-center text-center items-center gap-3">
-        <h1 className="text-[#85502E] sm:text-5xl lg:text-5xl text-3xl">
+      <img src={background} className="w-full shadow-sm h-screen" alt="" />
+      <div className="absolute sm:bottom-8 lg:bottom-0 bottom-6 w-full h-full flex flex-col justify-center text-center items-center gap-3">
+        <h1 className="text-300 sm:text-5xl lg:text-5xl text-3xl">
           {props.title}
         </h1>
-        <p className="text-primary text-lg xl:w-[90%] w-[60%]">
+        <p className="text-300 text-lg xl:w-[90%] w-[60%]">
           {props.description}
         </p>
         <Button
